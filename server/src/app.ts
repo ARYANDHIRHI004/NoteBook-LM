c32c2dd35d0fbec347b4579518ae2202f911dba6
ARYANDHIRHI004 <aryandhirhi004@gmail.com>
2026-09-11 13:14:47 +0530

    feat: complete better auth implementataion

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,11 @@
+import { toNodeHandler } from "better-auth/node";
 import express from "express";
+import { auth } from "./config/auth.js";
 
 const app = express();
+
+app.get("/api/auth/{*any}", toNodeHandler(auth));
+app.post("/api/auth/{*any}", toNodeHandler(auth));
 
 app.use(express.json());
 
